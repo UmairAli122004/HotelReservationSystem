55069ae5f35a8214db6649772c9c0cddaeca73c9
UmairAli122004 <aliumair7379@gmail.com>
2025-11-27 20:26:51 +0530

    Update HotelReservationSystemProject.java

--- HotelReservationSystemProject.java
+++ HotelReservationSystemProject.java
@@ -1,5 +1,3 @@
-package JDBCJAVAproject;
-
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.util.Scanner;
@@ -284,3 +282,4 @@ public class HotelReservationSystemProject {
         }
     }
 }
+
